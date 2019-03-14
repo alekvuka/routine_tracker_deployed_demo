3ef1594da3b1b9b90ed7958ee94205f09936913b
alekvuka <alekvuka@gmail.com>
2019-03-13 21:24:09 -0400

    add number_prompter and number_completed fields to routines table

--- db/migrate/20190313224642_create_routines.rb
+++ db/migrate/20190313224642_create_routines.rb
@@ -6,6 +6,9 @@ class CreateRoutines < ActiveRecord::Migration[5.2]
       t.integer :start_time
       t.integer :end_time
 
+      t.integer :number_completed
+      t.integer :number_prompted
+
       t.timestamps
     end
   end
