179ca5aaa2224d1c9b444defedd316d5c78a3d98
alekvuka <alekvuka@gmail.com>
2019-03-17 16:17:58 -0400

    add protect_from_forgery in ApplicationsController

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,7 +1,6 @@
 class RoutinesController < ApplicationController
 
   def index
-
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,20 +3,20 @@ class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :routine
 
-  def self.unique_names
+  #has_many users
+  #has_many routines_trheough users
 
+
+  def self.unique_names
     names = Array.new
     ret = Array.new
-
     Task.all.each do |task|
       if !names.include?(task.name)
         names << task.name
         ret << task
       end
     end
-
     ret
-
   end
 
 end
